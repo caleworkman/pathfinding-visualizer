3597b2aa1f846ccde36f7823be41235dc46847e6
Cale <caleworkman@gmail.com>
2020-06-03 16:50:25 -0400

    Add ability for grid to expand with window. Shrinking does not work.

--- src/views/App.js
+++ src/views/App.js
@@ -18,52 +18,36 @@ class App extends Component {
 
     this.gridRef = React.createRef();
 
-    this.handleResize = this.handleResize.bind(this);
+    this.getNumberColumns = this.getNumberColumns.bind(this);
+    this.getNumberRows = this.getNumberRows.bind(this);
 
+    this.handleResize = this.handleResize.bind(this);
     this.handleMouseDown = this.handleMouseDown.bind(this);
     this.handleMouseUp = this.handleMouseUp.bind(this);
     this.handleMouseOver = this.handleMouseOver.bind(this);
 
-    this.getNumberColumns = this.getNumberColumns.bind(this);
-    this.getNumberRows = this.getNumberRows.bind(this);
+    this.expandToNewGrid = this.expandToNewGrid.bind(this);
+    this.initializeNewGrid = this.initializeNewGrid.bind(this);
+
     this.randomizeStartAndFinish = this.randomizeStartAndFinish.bind(this);
     this.resetGrid = this.resetGrid.bind(this);
   }
 
   componentDidMount() {
     window.addEventListener("resize", this.handleResize);
-    const grid = this.initializeNewGrid();
     this.setState({ grid: this.initializeNewGrid() });
   }
 
   componentWillUnmount() {
-    window.removeventListener("resize", this.handleResize);
+    window.removeEventListener("resize", this.handleResize);
   }
 
-  initializeNewGrid() {
-    const height = this.gridRef.current.clientHeight;
-    const width = this.gridRef.current.clientWidth;
+  getNumberColumns() {
+    return this.state.grid[0].length;
+  }
 
-    const numberRows = Math.floor(height/CELL_SIDE_LENGTH);
-    const numberColumns = Math.floor(width/CELL_SIDE_LENGTH);
-
-    let newGrid = [];
-    for (var i=0; i<numberRows; i++) {
-      const row = [];
-      for (var j=0; j<numberColumns; j++) {
-        row.push({
-          row: i,
-          column: j,
-          type: null,
-          onMouseDown: this.handleMouseDown,
-          onMouseUp: this.handleMouseUp,
-          onMouseOver: this.handleMouseOver
-        });
-      }
-      newGrid.push(row);
-    }
-
-    return newGrid;
+  getNumberRows() {
+    return this.state.grid.length;
   }
 
   handleMouseDown(cell) {
@@ -93,57 +77,107 @@ class App extends Component {
   }
 
   handleResize() {
-    // Redraw the grid, but keep the other cells the same. Do this by adding
-    // new cells outside
+
+
+    // TODO: need to renumber the new grid based on its changes
+    this.setState({ grid: this.expandToNewGrid() });
+  }
+
+  initializeNewGrid() {
+    const height = this.gridRef.current.clientHeight;
+    const width = this.gridRef.current.clientWidth;
+
+    const numberRows = Math.floor(height/CELL_SIDE_LENGTH);
+    const numberColumns = Math.floor(width/CELL_SIDE_LENGTH);
+
+    let newGrid = [];
+    for (var i=0; i<numberRows; i++) {
+      const row = [];
+      for (var j=0; j<numberColumns; j++) {
+        row.push({
+          row: i,
+          column: j,
+          type: null,
+          onMouseDown: this.handleMouseDown,
+          onMouseUp: this.handleMouseUp,
+          onMouseOver: this.handleMouseOver
+        });
+      }
+      newGrid.push(row);
+    }
+
+    return newGrid;
+  }
+
+  expandToNewGrid() {
+
+    // Redraw the grid, but keep the other cells the same.
     const height = this.gridRef.current.clientHeight;
     const width = this.gridRef.current.clientWidth;
 
     const newNumRows = Math.floor(height/CELL_SIDE_LENGTH);
     const newNumColumns = Math.floor(width/CELL_SIDE_LENGTH);
 
-    const oldNumColumns = this.getNumberColumns();
     const oldNumRows = this.getNumberRows();
+    const oldNumColumns = this.getNumberColumns();
 
-    console.log(oldNumRows, newNumRows);
+    const diffRows = newNumRows - oldNumRows;
+    const diffColumns = newNumColumns - oldNumColumns;
 
-    const grid = this.state.grid.slice();
+    console.log(diffColumns);
 
-    // Add new rows if needed
-    if (oldNumRows > newNumRows) {
+    const newGrid = this.state.grid.slice();
+
+    if (diffRows <= -2) {
       // remove first and last rows from the old grid
-      grid.shift();
-      grid.pop();
-    } else if (newNumRows - oldNumRows >= 2) {
+      for (var i=diffRows; i<0; i+=2) {
+        newGrid.shift();
+        newGrid.pop();
+      }
+    } else if (diffRows >= 2) {
       // Add two new rows. Use old columns because we check for new columns next
-      grid.unshift(new Array(oldNumColumns));
-      grid.push(new Array(oldNumColumns));
+      for (var i=0; i<diffRows; i+=2) {
+        newGrid.unshift(new Array(oldNumColumns));
+        newGrid.push(new Array(oldNumColumns));
+      }
     }
 
-    if (oldNumColumns > newNumColumns) {
+    if (diffColumns <= -2) {
       // remove first and last column
-      for (var row=0; row<grid.length; row++) {
-        grid[row].shift();
-        grid[row].pop();
+      for (var row=0; row<newGrid.length; row+=2) {
+        newGrid[row].shift();
+        newGrid[row].pop();
       }
-    } else if (newNumColumns - oldNumColumns >= 2) {
+    } else if (diffColumns >= 2) {
       // Add two new columns to each row
-      for (var row=0; row<grid.length; row++) {
-        grid[row].unshift();
-        grid[row].push();
+      for (var row=0; row<newGrid.length; row++) {
+        for (var col=0; col<diffColumns; col+=2) {
+          newGrid[row].unshift({});
+          newGrid[row].push({});
+        }
       }
     }
 
-    // TODO: need to renumber the new grid based on its changes
+    for (var row=0; row<newGrid.length; row++) {
+      for (var col=0; col<newGrid[0].length; col++) {
+        let cell = newGrid[row][col];
+        if (cell) {
+          cell.row = row;
+          cell.column = col;
+        } else {
+          newGrid[row][col] = {
+            row: row,
+            column: col,
+            type: null,
+            onMouseDown: this.handleMouseDown,
+            onMouseUp: this.handleMouseUp,
+            onMouseOver: this.handleMouseOver
+          }
+        }
+      }
+    }
 
-    this.setState({ grid: grid });
-  }
-
-  getNumberColumns() {
-    return this.state.grid[0].length;
-  }
-
-  getNumberRows() {
-    return this.state.grid.length;
+    return newGrid;
   }
 
   randomizeStartAndFinish() {
